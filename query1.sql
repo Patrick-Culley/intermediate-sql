46e81c5dc42d4dd634e8071f921f5b269f436a77
patbc <culleyp@oregonstate.edu>
2023-02-04 02:18:23 -0600

    completed query1

--- query1.sql
+++ query1.sql
@@ -1,6 +1,39 @@
--- Write the query to create the 4 tables below.
+CREATE OR REPLACE TABLE client (
+    id int(11) AUTO_INCREMENT, 
+    first_name varchar(255) NOT NULL, 
+    last_name varchar(255) NOT NULL, 
+    email varchar(255) NOT NULL, 
+    PRIMARY KEY(id),
+    CONSTRAINT full_name UNIQUE(first_name, last_name)
+);
 
+CREATE OR REPLACE TABLE employee (
+    id int(11) AUTO_INCREMENT, 
+    first_name varchar(255) NOT NULL, 
+    last_name varchar(255) NOT NULL,
+    start_date DATE NOT NULL,
+    email varchar(255) NOT NULL,
+    PRIMARY KEY(id),
+    CONSTRAINT full_name UNIQUE(first_name, last_name)
+);
 
+CREATE OR REPLACE TABLE project (
+    id int(11) AUTO_INCREMENT, 
+    title varchar(255) NOT NULL UNIQUE, 
+    comments TEXT, 
+    cid int, 
+    PRIMARY KEY(id), 
+    FOREIGN KEY(cid) REFERENCES client(id)
+);
+
+CREATE OR REPLACE TABLE works_on (
+    pid int,
+    eid int, 
+    due_date DATE NOT NULL, 
+    FOREIGN KEY(pid) REFERENCES project(id), 
+    FOREIGN KEY(eid) REFERENCES employee(id), 
+    PRIMARY KEY(eid, pid)
+);
 
 -- Leave the queries below untouched. These are to test your submission correctly.
 -- Test that the tables were created
